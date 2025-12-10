1730916c3f7be78e22768b2436ed7644e556d801
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-12-10 10:42:14 -0300

    change id tables to uuid

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,42 +1,44 @@
-import { boolean, integer, pgEnum, pgTable, timestamp, uniqueIndex, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgEnum, pgTable, timestamp, uniqueIndex, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const studyStatusEnum = pgEnum("study_status", ["em_andamento", "finalizado",]);
 
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   username: varchar({ length: 255 }).notNull().unique(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar({ length: 255 }).notNull(),
   profileImage: varchar({ length: 255 }),
-  role: varchar({ length: 50 }).default('user').notNull(),
+  role: varchar({ length: 50 }).default("user").notNull(),
   createdAt: timestamp().defaultNow().notNull(),
-})
+});
 
 export const studiesTable = pgTable("studies", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   type: varchar({ length: 255 }),
   link: varchar({ length: 500 }),
   description: varchar({ length: 1000 }),
-  status: varchar({ length: 50 }).default("em_andamento").notNull(),
+  status: studyStatusEnum("status").default("em_andamento").notNull(),
   progress: integer().default(0).notNull(),
-  userId: integer().notNull().references(() => usersTable.id, { onDelete: "cascade" }),
+  userId: uuid("userId").notNull().references(() => usersTable.id, { onDelete: "cascade" }),
   createdAt: timestamp().defaultNow().notNull(),
   updatedAt: timestamp().defaultNow().notNull().$onUpdate(() => new Date()),
 });
 
 export const tasksTable = pgTable("tasks", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().defaultRandom(),
   title: varchar({ length: 255 }).notNull(),
   link: varchar({ length: 500 }),
   done: boolean().default(false).notNull(),
-  studyId: integer().notNull().references(() => studiesTable.id, { onDelete: "cascade" }),
+  studyId: uuid("studyId").notNull().references(() => studiesTable.id, { onDelete: "cascade" }),
   createdAt: timestamp().defaultNow().notNull(),
   finishIn: timestamp(),
   finishedAt: timestamp(),
 });
 
 export const quizzesTable = pgTable("quizzes", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().defaultRandom(),
   title: varchar({ length: 255 }).notNull().unique(),
   description: varchar({ length: 1000 }),
   imageUrl: varchar({ length: 500 }),
@@ -45,26 +47,26 @@ export const quizzesTable = pgTable("quizzes", {
 });
 
 export const questionsTable = pgTable("questions", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().defaultRandom(),
   question: varchar({ length: 1000 }).notNull(),
-  quizId: integer().notNull().references(() => quizzesTable.id, { onDelete: "cascade" }),
+  quizId: uuid("quizId").notNull().references(() => quizzesTable.id, { onDelete: "cascade" }),
   createdAt: timestamp().defaultNow().notNull(),
   updatedAt: timestamp().defaultNow().notNull().$onUpdate(() => new Date()),
 });
 
 export const alternativesTable = pgTable("alternatives", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().defaultRandom(),
   text: varchar({ length: 1000 }).notNull(),
   isCorrect: boolean().default(false).notNull(),
-  questionId: integer().notNull().references(() => questionsTable.id, { onDelete: "cascade" }),
+  questionId: uuid("questionId").notNull().references(() => questionsTable.id, { onDelete: "cascade" }),
   createdAt: timestamp().defaultNow().notNull(),
   updatedAt: timestamp().defaultNow().notNull().$onUpdate(() => new Date()),
 });
 
 export const userQuizzesTable = pgTable("user_quizzes", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  userId: integer().notNull().references(() => usersTable.id, { onDelete: "cascade" }),
-  quizId: integer().notNull().references(() => quizzesTable.id, { onDelete: "cascade" }),
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("userId").notNull().references(() => usersTable.id, { onDelete: "cascade" }),
+  quizId: uuid("quizId").notNull().references(() => quizzesTable.id, { onDelete: "cascade" }),
   unlockedAt: timestamp().defaultNow().notNull(),
 }, (table) => {
   return {
@@ -73,9 +75,9 @@ export const userQuizzesTable = pgTable("user_quizzes", {
 });
 
 export const quizAttemptsTable = pgTable("quiz_attempts", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  userId: integer().notNull().references(() => usersTable.id, { onDelete: "cascade" }),
-  quizId: integer().notNull().references(() => quizzesTable.id, { onDelete: "cascade" }),
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("userId").notNull().references(() => usersTable.id, { onDelete: "cascade" }),
+  quizId: uuid("quizId").notNull().references(() => quizzesTable.id, { onDelete: "cascade" }),
   startedAt: timestamp().defaultNow().notNull(),
   finishedAt: timestamp(),
   score: integer(),
