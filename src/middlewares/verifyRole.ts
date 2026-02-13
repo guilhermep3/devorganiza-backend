93253cf2a896680b2a7fd2fd1820b405793629bc
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2026-02-13 15:49:31 -0300

    feat: add a array of allowed roles in middleware

--- src/middlewares/verifyRole.ts
+++ src/middlewares/verifyRole.ts
@@ -3,8 +3,9 @@ import type { ExtendedRequest } from "../types/request.js";
 
 export const verifyRole = (req: ExtendedRequest, res: Response, next: NextFunction) => {
   const role = req.userRole;
+  const allowedRoles = ["admin", "dev"];
 
-  if (!role || role !== "admin") {
+  if (!role || !allowedRoles.includes(role)) {
     res.status(403).json({ error: "Acesso negado, permissão insuficiente" });
     return;
   }
